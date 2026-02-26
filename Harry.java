e92c63662124b3ae92cb9737c74cd6161b45b69d
riddhi <rs9485@srmist.edu.in>
2026-02-27 00:24:21 +0530

    coded w1 and w2 problems level 1

--- Harry.java
+++ Harry.java
@@ -1,7 +1,7 @@
 class Harry {
     public static void main(String[] args) {
-        int birthYear = 2000;
-        int currentYear = 2024;
-        System.out.println("Harry's age in 2024 is " + (currentYear - birthYear));
+    int birthYear = 2000;
+    int currentYear = 2024;
+    System.out.println("Harry's age in 2024 is " + (currentYear - birthYear));
     }
 }
