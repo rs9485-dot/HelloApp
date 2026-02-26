48215cd1a6600165c07861339a660fb5289f64d0
riddhi <rs9485@srmist.edu.in>
2026-02-27 00:25:07 +0530

    coded w1 and w2 problems level 1

--- Sam.java
+++ Sam.java
@@ -3,7 +3,6 @@ class Sam {
         int maths = 94;
         int physics = 95;
         int chemistry = 96;
-
         double average = (maths + physics + chemistry) / 3.0;
 
         System.out.println("Sam’s average mark in PCM is " + average);
